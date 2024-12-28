f23d3fac44b2f85964b79d9ce212f1afedff489e
pietro <pietro.peerani@gmail.com>
2024-12-28 15:08:02 +0100

    update: CustomVector class

--- vector.py
+++ vector.py
@@ -2,39 +2,44 @@ import math
 from rapidfuzz.fuzz import partial_ratio
 
 class VectorCompare:
-  def magnitude(self,concordance):
-    if type(concordance) != dict:
-      raise ValueError('Supplied Argument should be of type dict')
-    total = 0
-    for word,count in concordance.iteritems():
-      total += count ** 2
-    return math.sqrt(total)
+    def magnitude(self, concordance):
+        if not isinstance(concordance, dict):
+            raise ValueError('Supplied Argument should be of type dict')
+        
+        total = 0
+        for count in concordance.values():
+            total += count ** 2
+        
+        return math.sqrt(total)
 
-  def relation(self,concordance1, concordance2):
-    if type(concordance1) != dict:
-      raise ValueError('Supplied Argument 1 should be of type dict')
-    if type(concordance2) != dict:
-      raise ValueError('Supplied Argument 2 should be of type dict')
-    relevance = 0
-    topvalue = 0
-    for word, count in concordance1.iteritems():
-      if concordance2.has_key(word):
-        topvalue += count * concordance2[word]
-    if (self.magnitude(concordance1) * self.magnitude(concordance2)) != 0:
-      return topvalue / (self.magnitude(concordance1) * self.magnitude(concordance2))
-    else:
-      return 0
+    def relation(self, concordance1, concordance2):
+        if not isinstance(concordance1, dict):
+            raise ValueError('Supplied Argument 1 should be of type dict')
+        if not isinstance(concordance2, dict):
+            raise ValueError('Supplied Argument 2 should be of type dict')
+        
+        topvalue = 0
+        for word, count in concordance1.items():
+            if word in concordance2:
+                topvalue += count * concordance2[word]
+        
+        magnitude1 = self.magnitude(concordance1)
+        magnitude2 = self.magnitude(concordance2)
+        
+        if magnitude1 * magnitude2 != 0:
+            return topvalue / (magnitude1 * magnitude2)
+        else:
+            return 0
 
-  def concordance(self,document):
-    if type(document) != str:
-      raise ValueError('Supplied Argument should be of type string')
-    con = {}
-    for word in document.split(' '):
-      if con.has_key(word):
-        con[word] = con[word] + 1
-      else:
-        con[word] = 1
-    return con
+    def concordance(self, document):
+        if not isinstance(document, str):
+            raise ValueError('Supplied Argument should be of type string')
+        
+        con = {}
+        for word in document.split():
+            con[word] = con.get(word, 0) + 1
+        
+        return con
 
 class CustomVectorCompare:
     def magnitude(self, concordance):
