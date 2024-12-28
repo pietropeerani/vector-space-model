4f027fd3cd62a8a0d0f3ddb5d25dda9349300b62
pietro <pietro.peerani@gmail.com>
2024-12-28 15:12:57 +0100

    add: custom file function

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
 from vector import VectorCompare, CustomVectorCompare
 import argparse
+import json
+
+def process_file(file_path):
+    """
+    Process the input file based on its extension:
+    - If it's a .json file, read an array of strings.
+    - If it's a .txt file, split the text into sentences.
+    """
+    if file_path.endswith(".json"):
+        try:
+            with open(file_path, "r", encoding="utf-8") as f:
+                data = json.load(f)
+                if isinstance(data, list) and all(isinstance(item, str) for item in data):
+                    return data
+                else:
+                    print("Error: JSON file must contain an array of strings.")
+                    exit(1)
+        except Exception as e:
+            print(f"Error reading JSON file: {e}")
+            exit(1)
+    elif file_path.endswith(".txt"):
+        try:
+            with open(file_path, "r", encoding="utf-8") as f:
+                text = f.read()
+                # Split the text into phrases based on periods
+                phrases = [sentence.strip() for sentence in text.split('.') if sentence.strip()]
+                return phrases
+        except Exception as e:
+            print(f"Error reading TXT file: {e}")
+            exit(1)
+    else:
+        print("Error: Unsupported file type. Supported types are .json and .txt.")
+        exit(1)
 
 parser = argparse.ArgumentParser(
-    description="Vector Space Model (VSM).",
+    description="Vector Space Model (VSM) with support for .json and .txt files."
 )
 
 parser.add_argument(
     "file",
-    nargs="?",
-    help="The path to the file to process",
+    help="The path to the file to process. Supported extensions: .json (array of strings), .txt (plain text).",
 )
 parser.add_argument(
     "-c",
@@ -25,6 +57,7 @@ parser.add_argument(
 
 args = parser.parse_args()
 
+# Check if a file path is provided
 if not args.file:
     parser.print_help()
     exit()
@@ -32,18 +65,8 @@ if not args.file:
 # Determine which class to use based on the -c flag
 v = CustomVectorCompare() if args.custom else VectorCompare()
 
-print(f"Custom Algorithm: {args.custom}")
-print(f"File Path: {args.file}")
-
-documents = [
-    '''At Scale You Will Hit Every Performance Issue I used to think I knew a bit about performance scalability and how to keep things trucking when you hit large amounts of data Truth is I know diddly squat on the subject since the most I have ever done is read about how its done To understand how I came about realising this you need some background''',
-    '''Richard Stallman to visit Australia Im not usually one to promote events and the like unless I feel there is a genuine benefit to be had by attending but this is one stands out Richard M Stallman the guru of Free Software is coming Down Under to hold a talk You can read about him here Open Source Celebrity to visit Australia''',
-    '''MySQL Backups Done Easily One thing that comes up a lot on sites like Stackoverflow and the like is how to backup MySQL databases The first answer is usually use mysqldump This is all fine and good till you start to want to dump multiple databases You can do this all in one like using the all databases option however this makes restoring a single database an issue since you have to parse out the parts you want which can be a pain''',
-    '''Why You Shouldnt roll your own CAPTCHA At a TechEd I attended a few years ago I was watching a presentation about Security presented by Rocky Heckman read his blog its quite good In it he was talking about security algorithms The part that really stuck with me went like this''',
-    '''The Great Benefit of Test Driven Development Nobody Talks About The feeling of productivity because you are writing lots of code Think about that for a moment Ask any developer who wants to develop why they became a developer One of the first things that comes up is I enjoy writing code This is one of the things that I personally enjoy doing Writing code any code especially when its solving my current problem makes me feel productive It makes me feel like Im getting somewhere Its empowering''',
-    '''Setting up GIT to use a Subversion SVN style workflow Moving from Subversion SVN to GIT can be a little confusing at first I think the biggest thing I noticed was that GIT doesnt have a specific workflow you have to pick your own Personally I wanted to stick to my Subversion like work-flow with a central server which all my machines would pull and push too Since it took a while to set up I thought I would throw up a blog post on how to do it''',
-    '''Why CAPTCHA Never Use Numbers 0 1 5 7 Interestingly this sort of question pops up a lot in my referring search term stats Why CAPTCHAs never use the numbers 0 1 5 7 Its a relativity simple question with a reasonably simple answer Its because each of the above numbers are easy to confuse with a letter See the below''',
-]
+# Process the input file
+documents = process_file(args.file)
 
 index = []
 for item in documents:
